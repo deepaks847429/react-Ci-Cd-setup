e78d35f062e909a0b3be3267a34985f52ed61c8d
Deepak Kumar <deepaks9631342845@gmail.com>
2025-03-26 21:34:15 +0530

    build the application

--- src/assets/setupTests.js
+++ src/assets/setupTests.js
@@ -1,2 +1,2 @@
 // src/setupTests.js
-import '@testing-library/jest-dom';
+i//mport '@testing-library/jest-dom';
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import {configDefaults} from 'jest-config'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.js',
-    exclude:[...configDefaults.exclude, 'e2e/*'] // Add this line if you have a setup file
-  },
+
 })
